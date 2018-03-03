e466c5218906a6ef3f27bb4c3ddb5a3498d1dd8e
Reagan Wood <reaganwood1@cougar.cs.ksu.edu>
2018-03-03 16:54:09 -0600

    Added initial pintos files

---
+++ pintos/src/threads/build/tests/threads/my-alarm.d
@@ -0,0 +1,6 @@
+tests/threads/my-alarm.o: ../../tests/threads/my-alarm.c \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/kernel/stdio.h ../../tests/threads/tests.h \
+ ../../threads/malloc.h ../../threads/synch.h ../../lib/kernel/list.h \
+ ../../threads/thread.h ../../devices/timer.h ../../lib/round.h
