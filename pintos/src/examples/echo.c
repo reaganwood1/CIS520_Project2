a2e3c1a2cecc6360c0f556a991b3b5693fd68ce2
Carrington Cooper <carrington10@cougar.cs.ksu.edu>
2018-03-14 05:23:22 -0500

    accepts argument and prin hello world

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <syscall.h>
-
+// passing parameter to the register before invoking the funciton
 void prints(const char *s){
-    char  next_char ;
+  char  next_char ;
   for(;  *s; s++){
     next_char  = *s;
     __asm__ __volatile__ \
-      (" movl $65, %ebx\n\
-      int $0x30");
-      /*
-         :  no out put 
+      (" int $0x30"
+        :/*no out put from the rgister */
         : "b" (next_char)
-        :"%ebx"
-
-      );*/
-  }
+        :
+        );
+    }
 
 }
 int
 main (int argc, char **argv)
 {
   int i;
-//__asm__ __volatile__("int $0x30");
 prints("hello world");
 for(;;);
 /*
--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -6,16 +6,22 @@
 
 static void syscall_handler (struct intr_frame *);
 
-void
-syscall_init (void)
+void syscall0xff(struct intr_frame *f)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+
+printf("%c",f->ebx);
+
+}
+
+void
+syscall_init(void){
+  intr_register_int (0x30,3,INTR_ON,syscall_handler,"syscall");
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  printf("%c",f->ebx);
-  thread_exit ();
+  syscall0xff(f);
+
+//  thread_exit ();
 }
