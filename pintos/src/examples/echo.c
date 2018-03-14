5821fae6e6c462a0c64c91d1b86fd6189356f005
Carrington Cooper <carrington10@cougar.cs.ksu.edu>
2018-03-13 19:31:10 -0500

    currently prints out a

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <syscall.h>
 
+void prints(const char *s){
+    char  next_char ;
+  for(;  *s; s++){
+    next_char  = *s;
+    __asm__ __volatile__ \
+      (" movl $65, %ebx\n\
+      int $0x30");
+      /*
+         :  no out put 
+        : "b" (next_char)
+        :"%ebx"
+
+      );*/
+  }
+
+}
 int
 main (int argc, char **argv)
 {
   int i;
 //__asm__ __volatile__("int $0x30");
-  for(;;);
+prints("hello world");
+for(;;);
 /*
   for (i = 0; i < argc; i++)
   // this is the user space printf
--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -7,14 +7,15 @@
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
+  printf("%c",f->ebx);
   thread_exit ();
 }
