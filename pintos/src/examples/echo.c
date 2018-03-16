b02d43d052107545212123496eac896be60ae6f6
Reagan Wood <reaganwood1@cougar.cs.ksu.edu>
2018-03-16 11:34:42 -0500

    Added files

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -5,15 +5,16 @@
 int
 main (int argc, char **argv)
 {
+	
   int i;
+  /*
 printf("arg <%d>\n", argc);
 printf("argv[0] <%s>\n", argv[0]);
+*/
 
-/*
   for (i = 0; i < argc; i++)
   // this is the user space printf
   printf ("%s ", argv[i]);
   printf ("\n");
-*/
   return EXIT_SUCCESS;
 }
